bc8ea6dba77de3e2b6d698f25d924853d354a832
Noam <noambar2845@gmail.com>
2023-05-17 17:22:15 +0300

    google login leftout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,126 +1,122 @@
-import { useEffect, useState } from "react"
-import { useDispatch } from "react-redux"
-import { useNavigate } from "react-router-dom"
-import { onLogin, onSignup } from "../store/actions/user.action"
-import jwt_decode from "jwt-decode"
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { onLogin } from "../store/actions/user.action";
+import jwt_decode from "jwt-decode";
 
-const googleLoginKey = '191073475183-96e548l2ufak98s6fpsfoh888ftbm7pp.apps.googleusercontent.com'
-
-
+const googleLoginKey =
+  "191073475183-96e548l2ufak98s6fpsfoh888ftbm7pp.apps.googleusercontent.com";
 
 export const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
 
-	const dispatch = useDispatch()
-	const navigate = useNavigate()
-	const [isError, setIsError] = useState('')
+  // useEffect(() => {
+  //   /* global google */
+  //   google.accounts.id.initialize({
+  //     client_id: googleLoginKey,
+  //     callback: handleCallbackResponse,
+  //   });
 
-	const [credentials, setCredentials] = useState({
-		username: '',
-		password: '',
-		email:''
-	})
+  //   google.accounts.id.renderButton(
+  //     document.getElementById("google-login-div"),
+  //     { theme: "outline", size: "large" }
+  //   );
+  // }, []);
 
+  // const handleCallbackResponse = (response) => {
+  //   console.log("Encoded JWT Id token :  " + response.credential);
+  //   let decodeInfo = jwt_decode(response.credential);
+  //   console.log(decodeInfo);
+  //   dispatch(
+  //     onLogin({
+  //       ...credentials,
+  //       username: decodeInfo.name,
+  //       email: decodeInfo.email,
+  //     })
+  //   );
+  //   navigate("/");
+  // };
 
-	useEffect(() => {
-		/* global google */
-		google.accounts.id.initialize({
-			client_id: googleLoginKey,
-			callback: handleCallbackResponse
-		})
+  const handleChange = (ev) => {
+    let field = ev.target.name;
+    let value = ev.target.value;
+    setCredentials({ ...credentials, [field]: value });
+  };
 
-		google.accounts.id.renderButton(
-			document.getElementById('google-login-div'),
-			{theme:'outline', size:'large'}
-		)
-	}, [])
+  const onLoginUser = async () => {
+    if (
+      !credentials.username.length ||
+      !credentials.password.length
+      // !credentials.fullname.length
+    )
+      return console.log("empty fields");
+    dispatch(onLogin(credentials));
+    resetCredentials();
+    navigate(`/`);
+  };
 
-	const handleCallbackResponse = (response) => {
-		console.log("Encoded JWT Id token :  " + response.credential);
-		let decodeInfo = jwt_decode(response.credential)
-		console.log(decodeInfo);
-		dispatch(onLogin({...credentials , username:decodeInfo.name, email:decodeInfo.email}))
-		navigate('/')
-	}
+  const resetCredentials = () => {
+    setCredentials({
+      username: "",
+      password: "",
+      email: "",
+    });
+  };
 
-	const handleChange = (ev) => {
-		let field = ev.target.name
-		let value = ev.target.value
-		setCredentials({ ...credentials, [field]: value })
-	}
+  return (
+    <section className="sign-up-container">
+      <header className="sign-up-header">
+        <span>
+          <i
+            class="fa-brands fa-spotify"
+            style={{ height: "56px", width: "56px" }}
+          ></i>{" "}
+          Mellofy
+        </span>
+      </header>
+      <section className="sign-up-main">
+        <h1 style={{ fontSize: "24px" }}>
+          Sign up for free to start listening.
+        </h1>
+        {/* <section className="sign-up-platforms">
+          <button className="util-btn facebook">CONTINUE WITH FACEBOOK</button>
+          <div id="google-login-div"></div>
+        </section>
+        <span className="sign-up-or">
+          <hr />
+          <span>or</span>
+          <hr />
+        </span> */}
 
-	const onLoginUser = async () => {
-		if (
-			!credentials.username.length ||
-			!credentials.password.length
-			// !credentials.fullname.length
-		) return console.log('empty fields')
-		dispatch(onLogin(credentials))
-		// let newScrum = await boardService.getBoardForGuest()
-		// let newUser = { ...credentials, boards: [newScrum._id], starred: [newScrum._id] }
-		// newUser = await userService.update(newUser)
-		// await userService.saveLocalUser(newUser)
-		// dispatch(setCurrUser(newUser))
-		resetCredentials()
-		navigate(`/`)
-	}
-
-	const handleError = () => {
-		setIsError('show')
-		setTimeout(setIsError, 2000, '')
-	}
-
-	const resetCredentials = () => {
-		setCredentials({
-			username: '',
-			password: '',
-			email: ''
-		})
-	}
-
-	return (
-		<section className="sign-up-container">
-			<header className="sign-up-header">
-				<span><i class="fa-brands fa-spotify" style={{ height: '56px', width: '56px' }}></i> Mellofy</span>
-			</header>
-			<section className="sign-up-main">
-				<h1 style={{ fontSize: '24px' }}>Sign up for free to start listening.</h1>
-				<section className="sign-up-platforms">
-					<button className="util-btn facebook">CONTINUE WITH FACEBOOK</button>
-					{/* <button className="util-btn apple">continue with apple</button> */}
-					{/* <button className="util-btn google">CONTINUE WITH GOOGLE</button> */}
-					<div id='google-login-div'></div>
-				</section>
-				<span className='sign-up-or'><hr /><span>or</span><hr /></span>
-				{/* <h3>What's your name?</h3>
-				<input
-					type="text"
-					name="fullname"
-					value={credentials.fullname}
-					placeholder="Fullname"
-					onChange={handleChange}
-					required
-				/> */}
-				<h3>UserName</h3>
-				<input
-					type="text"
-					name="username"
-					value={credentials.username}
-					placeholder="Username"
-					onChange={handleChange}
-					required
-				/>
-				<h3>Password</h3>
-				<input
-					type="password"
-					name="password"
-					value={credentials.password}
-					placeholder="Password"
-					onChange={handleChange}
-					required
-				/>
-				<button className="signup-btn util-btn" onClick={onLoginUser}>Login</button>
-			</section>
-		</section>
-	)
-}
+        <h3>UserName</h3>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          placeholder="Username"
+          onChange={handleChange}
+          required
+        />
+        <h3>Password</h3>
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          placeholder="Password"
+          onChange={handleChange}
+          required
+        />
+        <button className="signup-btn util-btn" onClick={onLoginUser}>
+          Login
+        </button>
+      </section>
+    </section>
+  );
+};
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,154 +1,147 @@
-import { useEffect, useState } from "react"
-import { useDispatch } from "react-redux"
-import { uploadService } from "../services/upload.service"
-import { onSignup } from "../store/actions/user.action"
-import { importService } from "../services/import-img-service"
-import { useNavigate } from "react-router-dom"
-import jwt_decode from "jwt-decode"
-
-
-const googleLoginKey = '191073475183-96e548l2ufak98s6fpsfoh888ftbm7pp.apps.googleusercontent.com'
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { uploadService } from "../services/upload.service";
+import { onSignup } from "../store/actions/user.action";
+import { importService } from "../services/import-img-service";
+import { useNavigate } from "react-router-dom";
 
 export const SignUp = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-	const dispatch = useDispatch()
-	const navigate = useNavigate()
+  const [credentials, setCredentials] = useState({
+    fullname: "",
+    username: "",
+    password: "",
+    stations: [],
+    likedSongs: [],
+    imgUrl: "",
+    email: "",
+  });
 
+  // useEffect(() => {
+  // 	/* global google */
+  // 	google.accounts.id.initialize({
+  // 		client_id: googleLoginKey,
+  // 		callback: handleCallbackResponse
+  // 	})
 
-	const [credentials, setCredentials] = useState({
-		fullname: '',
-		username: '',
-		password: '',
-		stations: [],
-		likedSongs: [],
-		imgUrl: '',
-		email: ''
-	})
+  // 	google.accounts.id.renderButton(
+  // 		document.getElementById('google-login-div'),
+  // 		{ theme: 'outline', size: 'large' }
+  // 	)
+  // }, [])
 
+  //   const handleCallbackResponse = async (response) => {
+  //     let decodeInfo = jwt_decode(response.credential);
+  //     dispatch(
+  //       onSignup({
+  //         ...credentials,
+  //         fullname: decodeInfo.name,
+  //         username: decodeInfo.name,
+  //         email: decodeInfo.email,
+  //         imgUrl: decodeInfo.picture,
+  //       })
+  //     );
+  //     navigate(`/`);
+  //   };
 
-	useEffect(() => {
-		/* global google */
-		google.accounts.id.initialize({
-			client_id: googleLoginKey,
-			callback: handleCallbackResponse
-		})
+  const handleChange = async (ev) => {
+    let field;
+    let value;
+    if (ev.target.type === "file") {
+      field = "imgUrl";
+      let tempVal = await uploadService.uploadImg(ev);
+      value = tempVal.url;
+    } else {
+      field = ev.target.name;
+      value = ev.target.value;
+    }
+    setCredentials({ ...credentials, [field]: value });
+  };
 
-		google.accounts.id.renderButton(
-			document.getElementById('google-login-div'),
-			{ theme: 'outline', size: 'large' }
-		)
-	}, [])
+  const onSignUpNewUser = async () => {
+    if (
+      !credentials.username.length ||
+      !credentials.password.length ||
+      !credentials.fullname.length
+    )
+      return console.log("empty fields");
+    await dispatch(onSignup(credentials));
+    resetCredentials();
+    navigate(`/`);
+  };
 
-	const handleCallbackResponse = async (response) => {
-		// console.log("Encoded JWT Id token :  " + response.credential);
-		let decodeInfo = jwt_decode(response.credential)
-		console.log(decodeInfo);
-		// await setCredentials({ ...credentials, fullname: decodeInfo.name, username: decodeInfo.name, email: decodeInfo.email, imgUrl: decodeInfo.picture})
-		dispatch(onSignup({ ...credentials, fullname: decodeInfo.name, username: decodeInfo.name, email: decodeInfo.email, imgUrl: decodeInfo.picture}))
-		// resetCredentials()
-		navigate(`/`)
-	}
+  const resetCredentials = () => {
+    setCredentials({
+      fullname: "",
+      username: "",
+      password: "",
+      stations: [],
+      likedSongs: [],
+      imgUrl: "",
+      email: "",
+    });
+  };
 
-	const handleChange = async (ev) => {
-		let field
-		let value
-		if (ev.target.type === 'file') {
-			field = 'imgUrl'
-			let tempVal = await uploadService.uploadImg(ev)
-			value = tempVal.url
-		}
-		else {
-			field = ev.target.name
-			value = ev.target.value
-		}
-		setCredentials({ ...credentials, [field]: value })
-	}
-
-	const onSignUpNewUser = async () => {
-		if (
-			!credentials.username.length ||
-			!credentials.password.length ||
-			!credentials.fullname.length
-		) return console.log('empty fields')
-		await dispatch(onSignup(credentials))
-		// let newScrum = await boardService.getBoardForGuest()
-		// let newUser = { ...credentials, boards: [newScrum._id], starred: [newScrum._id] }
-		// newUser = await userService.update(newUser)
-		// await userService.saveLocalUser(newUser)
-		// dispatch(setCurrUser(newUser))
-		resetCredentials()
-		navigate(`/`)
-	}
-
-	const resetCredentials = () => {
-		setCredentials({
-			fullname: '',
-			username: '',
-			password: '',
-			stations: [],
-			likedSongs: [],
-			imgUrl: '',
-			email: ''
-		})
-	}
-
-	return (
-		<section className="sign-up-container">
-			<header className="sign-up-header">
-				<span><i class="fa-brands fa-spotify" style={{ height: '56px', width: '56px' }}></i> Mellofy</span>
-			</header>
-			<section className="sign-up-main">
-				<h1 style={{ fontSize: '24px' }}>Sign up for free to start listening.</h1>
-				<section className="sign-up-platforms">
-					<button className="util-btn facebook">CONTINUE WITH FACEBOOK</button>
-					{/* <button className="util-btn apple">continue with apple</button> */}
-					{/* <button className="util-btn google">CONTINUE WITH GOOGLE</button> */}
-					<div id='google-login-div'></div>
-				</section>
-				<span className='sign-up-or'><hr /><span>or</span><hr /></span>
-				<h1 className="sign-up-title">Sign up with your info</h1>
-				<h3>What's your name?</h3>
-				<input
-					type="text"
-					name="fullname"
-					value={credentials.fullname}
-					placeholder="Fullname"
-					onChange={handleChange}
-					required
-				/>
-				<h3>How should we call you?</h3>
-				<input
-					type="text"
-					name="username"
-					value={credentials.username}
-					placeholder="Username"
-					onChange={handleChange}
-					required
-				/>
-				<h3>Create a password</h3>
-				<input
-					type="password"
-					name="password"
-					value={credentials.password}
-					placeholder="Password"
-					onChange={handleChange}
-					required
-				/>
-				<label className="file-label" >
-					<img className="upload-img" src={importService.uploadSvg} />
-					<span className="file-label-txt">Upload a profile picture</span>
-					<input
-						className="file-input"
-						type="file"
-						name="imgUrl"
-						value={credentials.img}
-						onChange={handleChange}
-						style={{ visibility: "hidden" }}
-					/></label>
-
-				<button className="signup-btn util-btn" onClick={onSignUpNewUser}>Sign Up</button>
-			</section>
-		</section>
-
-	)
-}
+  return (
+    <section className="sign-up-container">
+      <header className="sign-up-header">
+        <span>
+          <i
+            class="fa-brands fa-spotify"
+            style={{ height: "56px", width: "56px" }}
+          ></i>{" "}
+          Mellofy
+        </span>
+      </header>
+      <section className="sign-up-main">
+        <h1 style={{ fontSize: "24px" }}>
+          Sign up for free to start listening.
+        </h1>
+        <h3>What's your name?</h3>
+        <input
+          type="text"
+          name="fullname"
+          value={credentials.fullname}
+          placeholder="Fullname"
+          onChange={handleChange}
+          required
+        />
+        <h3>How should we call you?</h3>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          placeholder="Username"
+          onChange={handleChange}
+          required
+        />
+        <h3>Create a password</h3>
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          placeholder="Password"
+          onChange={handleChange}
+          required
+        />
+        <label className="file-label">
+          <img className="upload-img" src={importService.uploadSvg} />
+          <span className="file-label-txt">Upload a profile picture</span>
+          <input
+            className="file-input"
+            type="file"
+            name="imgUrl"
+            value={credentials.img}
+            onChange={handleChange}
+            style={{ visibility: "hidden" }}
+          />
+        </label>
+        <button className="signup-btn util-btn" onClick={onSignUpNewUser}>
+          Sign Up
+        </button>
+      </section>
+    </section>
+  );
+};
